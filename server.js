8aa6beead0b8d106532e909f7b7a07067f8d8a72
danieloh91 <daniel.oh.91@gmail.com>
2016-07-15 15:09:30 -0700

    change JSON api endpoints

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // require express and other modules
 var express = require('express'),
-    app = express();
+    app = express(),
+    bodyParser = require('body-parser'),
+    db = require('./models');
 
 // parse incoming urlencoded form data
 // and populate the req.body object
@@ -45,16 +47,16 @@ app.get('/', function homepage(req, res) {
 app.get('/api', function api_index(req, res) {
   // TODO: Document all your api endpoints below
   res.json({
-    woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
-    message: "Welcome to my personal api! Here's what you need to know!",
-    documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    base_url: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    woops_i_has_forgot_to_document_all_my_endpoints: false, // CHANGE ME ;)
+    message: "Welcome to Daniel's personal api! Here's what you need to know!",
+    documentation_url: "https://github.com/danieloh91/express-personal-api/README.md", // CHANGE ME
+    base_url: "https://shrouded-brushlands-49491.herokuapp.com/", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "POST", path: "/api/something", description: "Collection of something"} // CHANGE ME
     ]
-  })
+  });
 });
 
 /**********
